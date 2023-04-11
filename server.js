246a4327ae94e155a90f841ea55fc4958230d9b8
Kirsten <amayak@email.unc.edu>
2023-04-10 21:16:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = args["port"]  || 5000;
-
+/*
 // Default API endpoint
 app.use((req, res) => {
   res.status(404).send('404 NOT FOUND');
 });
-
+*/
 // Check endpoint at /app/
 app.get('/app/', (req, res) => {
   res.status(200).send('200 OK');
