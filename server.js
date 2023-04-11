4f7b6be6f0bf807eeef3a2b13ce17dcf6348d1e5
Kirsten <amayak@email.unc.edu>
2023-04-10 20:25:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const args = minimist(process.argv.slice(2));
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
